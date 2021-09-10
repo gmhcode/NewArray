f7d5c2988efa940f8114ffb481ea5e61501cbe18
Greg <greg@passivelogic.com>
2021-09-10 10:08:13 -0600

    Created GrphNodeController, re-calibrated the periscope for some devices, created core data persistence and integrated it into the graph, created floor association with nodes

--- NewArray/ViewController.swift
+++ NewArray/ViewController.swift
@@ -12,8 +12,127 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var g = NewArray<String>(count: 3, type: "f")!
+        
+//        print(g)
+        
+        g.append(item: "a")
+        for (i,item) in g.enumerated() {
+            
+
+            print(item)
+        }
     }
 
 
 }
+struct StringIterator<Element>: IteratorProtocol {
 
+    let stringValue: Element
+    
+
+    
+    
+    mutating func next() -> Element? {
+        return stringValue
+    }
+}
+
+struct NewArray<Element>: Sequence {
+    
+    
+    __consuming func makeIterator() -> MyIterator {
+        MyIterator(array: self)
+    }
+    
+    
+    
+    struct MyIterator: IteratorProtocol {
+        
+        var currentIndex: Int = 0
+        var array: NewArray<Element>
+        
+        mutating func next() -> Element? {
+            
+            
+            if currentIndex > array.endIndex! {
+                currentIndex = 0
+                return nil
+            }
+            let element = array.pointer.baseAddress?.advanced(by: currentIndex).pointee
+            currentIndex += 1
+            return element
+        }
+        
+        
+    }
+    typealias Iterator = MyIterator
+    var count: Int
+    var currentIndex: Int = 0
+    var startIndex: Int?
+    var pointer: UnsafeMutableBufferPointer<Element>
+    var endIndex: Int?
+    
+    
+
+
+
+    
+    mutating func append(item: Element) {
+        guard endIndex != nil else {return}
+        
+        if endIndex == count - 1 {
+            count *= 2
+            
+            endIndex! += 1
+            let newPointer = UnsafeMutableBufferPointer<Element>.allocate(capacity: count)
+            newPointer.initialize(from: self)
+            newPointer.baseAddress?.advanced(by: endIndex!).pointee = item
+            
+
+            pointer.deallocate()
+            pointer = newPointer
+            
+            return
+        }
+        
+        
+        endIndex! += 1
+        pointer.baseAddress?.advanced(by: endIndex!).pointee = item
+    }
+    
+
+    
+    
+    init?(count: Int, type: Element) {
+
+        if count > 0 {
+            startIndex = 0
+            endIndex = count - 1
+            self.count = count
+        }else {
+            startIndex = nil
+            endIndex = nil
+            self.count = 0
+            return nil
+        }
+        pointer =  UnsafeMutableBufferPointer<Element>.allocate(capacity: count)
+        pointer.initialize(repeating: type)
+
+        
+    }
+    
+    subscript(position: Int) -> Element {
+        get {
+            return pointer.baseAddress!.advanced(by: position).pointee
+        }
+    }
+    //NEEDS TO HAVE APPEND
+
+
+    
+    
+
+    
+    
+}
